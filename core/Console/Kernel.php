b42fd305d9a8f686c0f7af4b5e57ff13175777d9
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-05-14 13:49:58 -0300

    Starting a fluent interface to CLI

--- core/Console/Kernel.php
+++ core/Console/Kernel.php
@@ -6,5 +6,29 @@ namespace Yui\Core\Console;
 
 class Kernel
 {
-	//...
+	public array $arguments = [];
+
+	public function __construct(array $arguments)
+	{
+		$this->arguments = $arguments;
+	}
+
+	public function boot()
+	{
+		if (count($this->arguments) < 2) {
+			(new ConsolePrinter)->text('Welcome to Yui Console', color: 'red', bold: true)->print();
+		}
+		// self::registerCommands();
+		// self::runCommand();
+	}
+
+	//Registrar os comandos
+	public function registerCommands()
+	{
+	}
+
+	//Executar o comando solicitado
+	public function runCommand()
+	{
+	}
 }
